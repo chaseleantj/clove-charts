34406a9a03b795bf44dc84f4b6a2f0c46deaf443
Chase Lean <chaseleanrocks@gmail.com>
2025-10-29 22:26:48 +0100

    Cleanup and initial poc

---
+++ src/components/charts/basic-chart.js
@@ -0,0 +1,10 @@
+'use client';
+
+import { useMemo, useEffect } from 'react';
+
+import BaseTestPlot from '@/components/plots/templates/test-plot';
+
+
+export default function TestChart() {
+    return <BaseTestPlot/>
+}
